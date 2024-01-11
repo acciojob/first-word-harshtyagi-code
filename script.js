fc0f13fe7fc861af7635ed034bd6cb027f47a7e9
Harsh Tyagi <69513043+harshtyagi-code@users.noreply.github.com>
2024-01-11 13:38:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function firstWord(s) {
   // your code here
+	if(!s){
+		return '';
+	}
+	const initialWord = s.split(' ');
+	let firstWord = initialWord[0];
+	return firstWord;
 }
-
 // Do not change the code below
 
 const s = prompt("Enter String:");
